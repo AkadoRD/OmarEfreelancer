e8cd4c37eaa9cba27936d2db10f55a0d845b84cd
OmarEfreelancer <akadord@gmail.com>
2025-09-28 18:53:42 -0400

    update

--- mail.php
+++ mail.php
@@ -2,21 +2,21 @@
 if(isset($_POST['send'])){
     $name=$_POST['name']
     $email=$_POST['email']
-    $messege=$_POST['message']
+    $message=$_POST['message']
 
     $to="omarencarnacion.r@gmail.com";
     $subject="New E-Mail from $email";
     $content="Name: $name \n";
     $content.="Email: $email \n";
-    $content.="Messege: $messege";
+    $content.="message: $message";
 
     $header="$email";
 
     $mail=mail($to,$subject,$content,$header);
     if($mail){
-        echo "<script>alert(Messege Sent)</script>";
+        echo "<script>alert(message Sent)</script>";
     }else{
-        echo "<script>alert(Messege Not Sent)</script>";
+        echo "<script>alert(message Not Sent)</script>";
     }
     }
 ?>
